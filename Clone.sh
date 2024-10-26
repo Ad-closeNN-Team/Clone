7edf487d927044ab71b7dc8fb46b6bd08462b778
Ad-closeNN <1709301095@qq.com>
2024-10-26 20:59:58 +0800

    统一时间、加入更多要 Clone 的仓库

--- Clone.sh
+++ Clone.sh
@@ -1,2 +1,9 @@
 git clone https://github.com/Hex-Dragon/PCL2.git
 git clone https://github.com/LTCatt/PCL2Help.git
+git clone https://github.com/LTCatt/PCL2Help.git
+git clone https://github.com/PCL-Community/PCLC-Meme.git
+git clone https://github.com/Hex-Dragon/LTW3.git
+git clone https://github.com/Hex-Dragon/Re-definition.git
+git clone https://github.com/Hex-Dragon/bot_luo_core.git
+git clone https://github.com/Hex-Dragon/BlacksmithReforged.git
+git clone https://github.com/Hex-Dragon/EnchantmentReforged.git
